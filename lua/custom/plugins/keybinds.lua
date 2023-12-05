2132d20322a7462c73041daf1724c68b5d7fc6b8
Konrad Kine <konrad.kine@tutanota.com>
2023-12-05 07:33:05 +0100

    feat: changed neotree to telescope file browser

--- lua/custom/plugins/keybinds.lua
+++ lua/custom/plugins/keybinds.lua
@@ -1,6 +1,18 @@
 return {
-  vim.keymap.set('n', '<leader>f', '<Cmd>:Neotree toggle<CR>', { silent = true }),
   vim.keymap.set('n', 'L', '<Cmd>:bnext<CR>', { silent = true }),
   vim.keymap.set('n', 'H', '<Cmd>:bprevious<CR>', { silent = true }),
   vim.keymap.set('n', 'W', '<Cmd>:bd<CR>', { silent = true }),
+  vim.keymap.set('n', ';', '<Cmd>:wincmd w<CR>', {silent = true}),
+  vim.api.nvim_set_keymap(
+    "n",
+    "<space>ff",
+    ":Telescope file_browser<CR>",
+    { noremap = true }
+  ),
+  vim.api.nvim_set_keymap(
+    "n",
+    "<space>fb",
+    ":Telescope file_browser path=%:p:h select_buffer=true<CR>",
+    { noremap = true }
+  )
 }
