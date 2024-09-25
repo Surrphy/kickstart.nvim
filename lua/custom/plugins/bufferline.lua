49950ca94bfb5a685f0307fa913102883bf83800
Konrad Kine <konrad.kine@tutanota.com>
2024-09-25 10:05:17 +0200

    oil+

---
+++ lua/custom/plugins/bufferline.lua
@@ -0,0 +1,10 @@
+return {
+  'akinsho/bufferline.nvim',
+  version = '*',
+  opts = {},
+  dependencies = 'nvim-tree/nvim-web-devicons',
+  setup = {},
+
+  vim.keymap.set('n', 'L', '<CMD>bnext<CR>', { desc = 'Open parent directory' }),
+  vim.keymap.set('n', 'H', '<CMD>bprevious<CR>', { desc = 'Open parent directory' }),
+}
