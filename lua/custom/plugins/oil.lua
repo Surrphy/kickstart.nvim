86ec524b095b1a9ddde555773fa7741ba345011b
Konrad Kine <konrad.kine@tutanota.com>
2024-10-22 18:50:08 +0200

    feat: some plugins

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -7,5 +7,5 @@ return {
   dependencies = { { 'echasnovski/mini.icons', opts = {} } },
   -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
 
-  vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' }),
+  vim.keymap.set('n', '-', '<CMD>Oil .<CR>', { desc = 'Open parent directory' }),
 }
